17568970873e33a4faae57f8e46c2e0c988f5ce0
Shane Reddy <shane2807@gmail.com>
2017-05-02 13:31:41 +0200

    Added in validations for appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :nurse
 
   validates_presence_of :appointment_date, :starttime, :endtime
+  validates_uniqueness_of :appointment_date, scope: [:starttime, :endtime], message: "This appointment is already taken"
+  validates_uniqueness_of :starttime, scope: [:appointment_date, :endtime], message: "This appointment is already taken"
+  validates_uniqueness_of :endtime, scope: [:appointment_date, :starttime], message: "This appointment is already taken"
 end
